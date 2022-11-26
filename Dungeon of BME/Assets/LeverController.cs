c3629cc6bc240d7e5af99c6c8ae10a086e8d0c43
bonna9 <b.boglarka.anna@gmail.com>
2022-11-26 01:04:51 +0100

    KeyChest/LeverDoor

---
+++ Dungeon of BME/Assets/LeverController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LeverController : MonoBehaviour
+{
+
+    public bool isOpen;
+    private Animator animator;
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeDoor()
+    {
+        isOpen = !isOpen;
+        animator.SetBool("isOpen", isOpen);
+        door.GetComponent<Animator>().SetBool("triggerOpen", isOpen);
+        door.GetComponent<BoxCollider2D>().isTrigger = isOpen;
+        if(isOpen)
+        {
+            door.GetComponent<SpriteRenderer>().sortingOrder = 3;
+        }
+        else {
+            door.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+    }
+}
